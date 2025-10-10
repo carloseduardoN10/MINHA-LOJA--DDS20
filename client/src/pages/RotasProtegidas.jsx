d3b3c6b97f0697442b54d980b82c9eb3d157643c
carloseduardoN10 <carlos.e.marques10@aluno.senai.br>
2025-10-10 19:25:26 -0300

    Testando

--- client/src/pages/RotasProtegidas.jsx
+++ client/src/pages/RotasProtegidas.jsx
@@ -9,13 +9,13 @@ const RotasProtegidas = () => {
   }
 
   return (
-    <div className="App">
+    <div className="App d-flex">
         {/* Barra de navegação fixa na lateral */}
-        <div>
+        <div className="position-fixed top-0 start-0 start-0 min-vh-100">
             <h1>Barra de navegação</h1>
         </div>
         {/* Conteúdo principal, dependendo de qual rota está */}
-        <div>
+        <div className="d-flex flex-column min-vh-100">
             <h1>Conteúdo principal</h1>
         </div>
       
